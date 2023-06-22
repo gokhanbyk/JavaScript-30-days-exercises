76907e11dc5d8b326686045707caedd422e2fa9b
Gökhan Bıyıkoğlu <105162003+gokhanbyk@users.noreply.github.com>
2023-06-22 13:12:53 +0300

    Uploading Day 7 exercises

--- Day 7 - Exercises/script.js
+++ Day 7 - Exercises/script.js
@@ -365,3 +365,317 @@ console.log(userIdGenerator()) // JİQirfı / 5IğiNSD
 
 
 // LEVEL 3
+
+
+let nOfChar = Number(prompt('how long do you want your id'))
+let nOfIds = Number(prompt('Hom many ids you want'))
+
+console.log('**');
+function userIdGeneratorByUser() { 
+    let characters = 'abcçdefgğhıijklmnoöprsştuivyzqwxABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZQWX1234567890'
+    let userId = ''
+    
+    for (let i = 0; i < nOfIds; i++) {
+        userId = ''
+        for (let i = 0; i < nOfChar; i++) {
+            let randomIndex = Math.floor(Math.random() * characters.length)
+            userId += characters.charAt(randomIndex)
+        }
+       console.log(userId);
+    }
+}
+console.log(userIdGeneratorByUser()) // zÜsRhDcL / c7xĞl21S / CcriğEgm
+
+
+function rgbColorGenerator() {
+    let random1 = Math.floor(Math.random() * 255)
+    let random2 = Math.floor(Math.random() * 255)
+    let random3 = Math.floor(Math.random() * 255)
+
+    return `rgb(${random1}, ${random2}, ${random3})`   
+}
+const h1 = document.querySelector('h1')
+h1.style.color = rgbColorGenerator()
+console.log(rgbColorGenerator()) // rgb(246, 94, 231) for example
+
+
+function arrayOfHexaColors(x) {
+    let hexaArray = []
+    for(let i = 0; i < x; i++) {
+        hexaArray.push(randomHexaNumberGenerator())
+    }
+    return hexaArray
+}
+console.log(arrayOfHexaColors(5)) // (5) ['#1F0B7F', '#FFcD8a', '#4fFe56', '#9DD21D', '#Dd949E']
+
+
+function arrayOfRgbColors(x) {
+    let rgbArray = []
+    for(let i = 0; i < x; i++) {
+        rgbArray.push(rgbColorGenerator())
+    }
+    return rgbArray
+}
+console.log(arrayOfRgbColors(5)) //  ['rgb(239, 25, 119)', 'rgb(210, 204, 220)', 'rgb(78, 150, 119)', 'rgb(75, 57, 146)', 'rgb(153, 246, 33)']
+
+
+function convertHexaToRgb(hexa) {
+    hexa = hexa.replace('#', '')
+
+    let r = parseInt(hexa.substring(0, 2), 16)
+    let g = parseInt(hexa.substring(2, 4), 16)
+    let b = parseInt(hexa.substring(4, 6), 16)
+
+    return `rgb(${r}, ${g}, ${b})`
+}
+let hexaColor = randomHexaNumberGenerator()
+let rgbColor = convertHexaToRgb(hexaColor)
+console.log(rgbColor) // rgb(58, 182, 173)
+
+
+function convertRgbToHexa(rgbColor) {
+    let rgbComponent = rgbColor.substring(4, rgbColor.length - 1).split(',')
+
+    let r = parseInt(rgbComponent[0].trim(), 10).toString(16).padStart(2, "0");
+    let g = parseInt(rgbComponent[1].trim(), 10).toString(16).padStart(2, "0");
+    let b = parseInt(rgbComponent[2].trim(), 10).toString(16).padStart(2, "0");
+  
+    let hexaColor = `#${r}${g}${b}`
+    return hexaColor
+} 
+rgbColor = rgbColorGenerator()
+hexaColor = convertRgbToHexa(rgbColor)
+console.log(hexaColor) // #d7809b
+
+
+hexaColor = randomHexaNumberGenerator()
+rgbColor = rgbColorGenerator()
+
+function generateColors(color, time) {
+    let hexa = ''
+    let rgb = ''
+
+    if(color == hexaColor && time == 1) {
+        hexa += randomHexaNumberGenerator()
+        return hexa
+    } else if (color == hexaColor && time > 1) {
+        for (let i = 0; i < time; i++) {
+         return arrayOfHexaColors(time)
+        }
+    } else if(color == rgbColor && time == 1) {
+        rgb += rgbColorGenerator()
+        return rgb
+    } else if (color == rgbColor && time > 1) {
+        for (let i = 0; i < time; i++) {
+            return arrayOfRgbColors(time)
+        }
+    } else {
+        return 'Wrong Value'
+    }
+
+}
+
+console.log(generateColors(hexaColor, 3)) // (3) ['#15ec92', '#97FFe1', '#186FF6']
+console.log(generateColors(hexaColor, 1)) // #FeC3B8
+console.log(generateColors(rgbColor, 1)) // rgb(168, 97, 22)
+console.log(generateColors(rgbColor, 3)) // ['rgb(62, 30, 57)', 'rgb(161, 192, 126)', 'rgb(219, 88, 18)']
+
+
+const shuffleArray = (array) => {
+    let currentIndex = array.length
+
+    while(currentIndex !== 0) {
+        let randomIndex = Math.floor(Math.random() * currentIndex)
+        currentIndex--;
+
+        let temporaryValue = array[currentIndex]
+        array[currentIndex] = array[randomIndex]
+        array[randomIndex] = temporaryValue
+    }
+    return array
+}
+
+let myArray = ['gökhan', 1, 2, 3, true, country = 'turkey']
+let shuffledArray = shuffleArray(myArray)
+console.log(shuffledArray) // (6) [true, 3, 2, 'gökhan', 'turkey', 1] // (6) [3, 'turkey', 2, 'gökhan', 1, true]
+
+
+const factorialNum = (number) => {
+    if(number == 0 || number == 1) {
+        return 1
+    } 
+    for(let i = number - 1; i > 0; i--)  {
+        number *= i
+    }
+    return number
+}
+console.log(factorialNum(5)) // 120
+
+
+function isEmpty(value) {
+    if(value === null || value === undefined) {
+        return true
+    }
+    if(typeof value === 'string' || Array.isArray(value)) {
+        return value.length === 0
+    }
+    if(typeof value === 'object') {
+        return Object.keys(value).length === 0
+    }
+    return false
+}
+// Example usage:
+console.log(isEmpty(null)) // true
+console.log(isEmpty(undefined)) // true
+console.log(isEmpty("")) // true
+console.log(isEmpty([])) // true
+console.log(isEmpty({})) // true
+
+console.log(isEmpty("Hello")) // false
+console.log(isEmpty([1, 2, 3])) // false
+console.log(isEmpty({ key: "value" })) // false
+
+
+function sum() {
+    let sum = 0
+    for(let i = 0; i < arguments.length; i++) {
+        sum += arguments[i]
+    }
+    return sum
+}
+console.log(sum(10, 20, 30 ,40)) // 100
+
+
+function sumOfArrayItems(array) {
+    let sum = 0
+    for(let i = 0; i < array.length; i++) {
+        if(typeof array[i] !== 'number') {
+            return 'this array is not include number'
+        }
+        sum += array[i]
+    }
+    return sum
+}
+let numbers = [10, 30, 5, 60, 15]
+
+console.log(sumOfArrayItems(shuffledArray)) // this array is not include number
+console.log(sumOfArrayItems(numbers)) // 120
+
+
+const average = (array) => {
+    let average = 0
+    let sum = 0
+    for(let i = 0; i < array.length; i++) {
+        if(typeof array[i] !== 'number') {
+            return 'this array is not include number'
+        }
+        sum += array[i]
+        average = sum / array.length
+    }
+    return average
+}
+console.log(average(numbers)) // 24
+
+
+function modifyArray(array) {
+    if(array.length < 5) {
+        return 'The length of array is not enough'
+    } else {
+        array.splice(4, 1, array[4].toUpperCase())
+        return array
+    }
+}
+
+let fruit = ['Avocado', 'Tomato', 'Potato','Mango', 'Lemon','Carrot']
+console.log(modifyArray(fruit)) // ['Avocado', 'Tomato', 'Potato', 'Mango', 'LEMON', 'Carrot']
+
+
+
+function isPrime(x) {
+    let isPrime = true
+    if(x == 1 || x == 0) {
+        isPrime = false
+    }
+    for(let i = 2; i < x; i++) {
+        if(x % i == 0) {
+            isPrime = false
+        }
+    }
+    if(isPrime) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(isPrime(13)) // true
+console.log(isPrime(30)) // false
+
+
+const allUniqueItems = (array) => {
+    let uniqueItems = new Set(array)
+    return uniqueItems.size === array.length
+}
+
+console.log(allUniqueItems(numbers)) // true
+let arrayNotUn = [1, 1, 2, 2, 3, 3]
+console.log(allUniqueItems(arrayNotUn)) // false
+
+
+function dataTypes(array) {
+    if(array.length == 0) {
+        return true
+    }
+
+    let firstType = typeof array[0]
+
+    for (let i = 1; i < array.length; i++) {
+        if(typeof array[i] !== firstType) {
+            return false
+        }
+    }
+    return true
+}
+console.log(dataTypes(numbers)) // true
+console.log(dataTypes(myArray)) // false
+
+
+function isValidVariable(variableName) {
+
+    if (!/^[a-zA-Z_$]/.test(variableName)) {
+        return 'invalid';
+    }
+    if (!/^[a-zA-Z0-9_$]+$/.test(variableName)) {
+        return 'invalid';
+    }
+    return 'valid'
+}
+console.log('***');
+console.log(isValidVariable('ahm?et')) // invalid
+console.log(isValidVariable('103mehmet')) // invalid
+console.log(isValidVariable('mehmet')) // valid
+console.log(isValidVariable('$special')) // invalid 
+
+
+function sevenRandomNumber() {
+    let array = []
+
+    for(let i = 0; i <= 7; i++) {
+        let random = Math.floor(Math.random() * 9)
+        if(array.includes(random)) {
+            continue
+        } else{
+            array.push(random)
+        }
+    }
+    return array
+}
+console.log(sevenRandomNumber()) // (5) [8, 4, 6, 5, 3]
+
+
+function reverseCountries() {
+    let copyCountriesData = countriesData.sort()
+    let reverseCountriesData = copyCountriesData.reverse()
+    return reverseCountriesData
+}
+console.log(reverseCountries())
