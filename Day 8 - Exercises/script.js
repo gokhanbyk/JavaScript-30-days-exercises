238fc9b99a1537924c41af6b2938bf54e988d241
Gökhan Bıyıkoğlu <105162003+gokhanbyk@users.noreply.github.com>
2023-06-22 16:09:07 +0300

    Update script.js

--- Day 8 - Exercises/script.js
+++ Day 8 - Exercises/script.js
@@ -27,3 +27,79 @@ console.log(dog.getDogInfo()) // My dog's name is Lady and her color is white sh
 
 
 // LEVEL 2
+
+// users => users.js
+
+function maxSkills(users) {
+    let maxSkills = 0
+    let mostSkillsPerson = ''
+
+    for (const user in users) {
+        let skillsLength = users[user].skills.length
+        if (skillsLength > maxSkills) {
+            maxSkills = skillsLength
+            mostSkillsPerson = user
+        }
+    }
+    return mostSkillsPerson
+}
+console.log(maxSkills(users)) // Asab
+
+
+function loggedInUsers(users) {
+    let loggedInUsers = 0
+    let higherPointUsers = 0
+    
+    for(const user in users) {
+        if(users[user].isLoggedIn){
+            loggedInUsers++
+        }
+        if(users[user].points >= 50) {
+            higherPointUsers++
+        }
+    }
+    return `logged in:${loggedInUsers} / higher than 50 point: ${higherPointUsers}`
+}
+console.log(loggedInUsers(users)) // logged in:2 / higher than 50 point: 3
+
+
+function mernDeveloper(users) {
+    let mernDeveloper = []
+    let isMernDeveloper = false
+    for(const merns in users) {
+        const developers = users[merns].skills
+        if(developers.includes('MongoDB', 'Express', 'React', 'Node')) {
+            isMernDeveloper = true
+            mernDeveloper.push(merns)
+        }
+    }
+    return mernDeveloper
+}
+console.log(mernDeveloper(users)) // (2) ['Asab', 'Paul']
+
+const copyUsers = Object.assign({}, users)
+
+copyUsers.Gökhan = {}
+console.log(copyUsers);
+
+
+const keys = Object.keys(users)
+console.log(keys) // (7) ['Alex', 'Asab', 'Brook', 'Daniel', 'John', 'Thomas', 'Paul']0: "Alex"1: "Asab"2: "Brook"3: "Daniel"4: "John"5: "Thomas"6: "Paul"length: 7[[Prototype]]: Array(0)
+
+const values = Object.values(users)
+console.log(values) 
+
+
+function countriesPrint(countries) {
+    for (const country in countries) {
+        const capital = countries[country].capital
+        const name = countries[country].name
+        const population = countries[country].population
+        const languages = countries[country].languages
+        return `Country name: ${name}, capital: ${capital}, languages: ${languages}, population: ${population}`
+    }
+}
+console.log(countriesPrint(countries)) // Country name: Afghanistan, capital: Kabul, languages: Pashto,Uzbek,Turkmen, population: 27657145
+
+
+// LEVEL 3
