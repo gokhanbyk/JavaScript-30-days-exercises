d6a211c8d7db3952135e51bc1c8aee4b4c4d7653
Gökhan Bıyıkoğlu <105162003+gokhanbyk@users.noreply.github.com>
2023-07-03 15:14:04 +0300

    Uploading Day 11 exercises

--- Day 11 - Exercises/script.js
+++ Day 11 - Exercises/script.js
@@ -86,3 +86,69 @@ findLessThan2() // Less than two skills: John : 1 skill
 
 
 // LEVEL 3
+
+function destructCountriesData() {
+    for(const {name, capital, population, languages} of countries_data) {
+        console.log(name, capital, population, languages)
+    }
+}
+destructCountriesData() // all countries have been printed
+
+
+const student1 = ['David', ['HTML', 'CSS', 'JS', 'React'], [98, 85, 90, 95]]
+
+let [name, skills, [, , jsScore, reactScore] ] = student1
+console.log(name, skills, jsScore, reactScore) // David (4) ['HTML', 'CSS', 'JS', 'React'] 90 95
+
+
+const students = [
+    ['David', ['HTML', 'CSS', 'JS', 'React'], [98, 85, 90, 95]],
+    ['John', ['HTML', 'CSS', 'JS', 'React'], [85, 80, 85, 80]]
+]
+
+function convertArrayToObject(arr) {
+    return arr.map(([name, skills, scores]) => {
+        return {name, skills, scores}
+    })
+}
+console.log(convertArrayToObject(students))
+
+const student = {
+    name: 'David',
+    age: 25,
+    skills: {
+      frontEnd: [
+        { skill: 'HTML', level: 10 },
+        { skill: 'CSS', level: 8 },
+        { skill: 'JS', level: 8 },
+        { skill: 'React', level: 9 }
+      ],
+      backEnd: [
+        { skill: 'Node',level: 7 },
+        { skill: 'GraphQL', level: 8 },
+      ],
+      dataBase:[
+        { skill: 'MongoDB', level: 7.5 },
+      ],
+      dataScience:['Python', 'R', 'D3.js']
+    }
+}
+
+const newStudent = JSON.parse(JSON.stringify(student)) // for deep copy
+
+const {skills: {frontEnd, backEnd, dataBase, dataScience}} = newStudent
+
+frontEnd.push({skill: 'Bootstrap', level: 8})
+console.log(frontEnd)
+
+backEnd.push({skill: 'Express', level: 9})
+console.log(backEnd)
+
+dataBase.push({skill: 'SQL', level: 8})
+console.log(dataBase)
+
+dataScience.push('SQL')
+console.log(dataScience)
+
+console.log(student)
+console.log(newStudent)
