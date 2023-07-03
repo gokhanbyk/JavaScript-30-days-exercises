35f32998c8786eaa070307315ef84059893a834d
Gökhan Bıyıkoğlu <105162003+gokhanbyk@users.noreply.github.com>
2023-07-03 14:08:46 +0300

    LEVEL 1 completed

--- Day 11 - Exercises/script.js
+++ Day 11 - Exercises/script.js
@@ -1,3 +1,27 @@
 // DAY 11 EXERCISES
 
 // LEVEL 1
+
+const constants = [2.72, 3.14, 9.81, 37, 100]
+
+const [e, pi, gravity, humanBodyTemp, waterBoilingTemp] = constants
+console.log(e, pi, gravity, humanBodyTemp, waterBoilingTemp) // 2.72 3.14 9.81 37 100
+
+const countries = ['Finland', 'Estonia', 'Sweden', 'Denmark', 'Norway']
+
+let [fin, est, sw, den, nor] = countries
+console.log(fin, est, sw, den, nor) // Finland Estonia Sweden Denmark Norway
+
+
+const rectangle = {
+  width: 20,
+  height: 10,
+  area: 200,
+  perimeter: 60
+}
+
+let {width, height, area, perimeter} = rectangle
+console.log(width, height, area, perimeter) // 20 10 200 60
+
+
+// LEVEL 2
