9e2fee0561b907352d6407381456d420ac61214f
Gökhan Bıyıkoğlu <105162003+gokhanbyk@users.noreply.github.com>
2023-07-07 12:28:44 +0300

    Update script.js

---
+++ Day 13 - Exercises/script.js
@@ -0,0 +1,38 @@
+// DAY 13 Exercise
+
+// LEVEL 1
+
+console.table(countries)
+
+const countriesObject = {
+    name: 'Turkey',
+    capital: 'Ankara',
+    languages: ['Turkish', 'English'],
+    population: 87057705,
+    flag: 'https://restcountries.eu/data/tur.svg',
+    currency: 'Turkish lira'
+}
+
+console.table(countriesObject)
+
+const countriesCapital = [
+    'Ankara',
+    'Rome',
+    'Berlin',
+    'London'
+]
+
+const country = [
+    'Turkey',
+    'Italy',
+    'Germany',
+    'England'
+]
+
+console.group('Countries')
+console.log(country)
+console.log(countriesCapital)
+console.groupEnd()
+
+
+// LEVEL 2
