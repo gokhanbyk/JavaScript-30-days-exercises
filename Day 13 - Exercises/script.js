18ca9c3056695914bb91c9757b00863d97af6182
Gökhan Bıyıkoğlu <105162003+gokhanbyk@users.noreply.github.com>
2023-07-07 12:35:49 +0300

    Uploading Day 13 Exercise

--- Day 13 - Exercises/script.js
+++ Day 13 - Exercises/script.js
@@ -44,3 +44,29 @@ console.error('Wrong password')
 
 
 // LEVEL 3
+
+const numbers = [1, 2, 3, 4, 5]
+
+console.time('While')
+let i = 0
+while(i < numbers.length) {
+    console.log(i)
+    i++
+}
+console.timeEnd('While') // While: 0.09814453125 ms
+
+console.time('For')
+for(let i = 0; i < numbers.length; i++) {
+    console.log(i)
+}
+console.timeEnd('For') // For: 0.0390625 ms
+
+console.time('For of')
+for(const num of numbers) {
+    console.log(num)
+}
+console.timeEnd('For of') // For of: 0.1240234375 ms
+
+console.time('forEach')
+numbers.forEach((num) => console.log(num))
+console.timeEnd('forEach') // forEach: 0.15625 ms
